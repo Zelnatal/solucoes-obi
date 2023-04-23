ec8d94ad39f7c5c6941c304acb853982a34dab88
Zelnatal <elnatan.honorato@gmail.com>
2023-04-22 23:45:13 -0300

    a

--- nivel2/2019/f1/soma.py
+++ nivel2/2019/f1/soma.py
@@ -1,12 +1,12 @@
 # questão: https://olimpiada.ic.unicamp.br/pratique/p2/2019/f1/soma/
 # nota: 25/100
 from array import array
-N, K = [int(x) for x in input().split()]
+N, K = map(int, input().split())
 prefix=array('i',[0])
-for i in input().split():
-    prefix.append(prefix[-1] + int(i))
+for i in map(int, input().split()):
+    prefix.append(prefix[-1] + i)
+    
 count = 0
-
 for i in range(N+1):
     for j in range(i+1,N+1):
         sub = prefix[j] - prefix[i]
