25869cc36720ca84ad9d59215aa6c58f65ccb9cc
Zelnatal <elnatan.honorato@gmail.com>
2023-07-09 13:04:00 -0300

    Ralouim

--- nivel2/2020/f1/ralouim_pilha.py
+++ nivel2/2020/f1/ralouim_pilha.py
@@ -1,14 +1,29 @@
 # questão: https://olimpiada.ic.unicamp.br/pratique/p2/2020/f1/ralouim/
-# nota: -/100
-# incompleto
+# nota: 90/100
 
 N = int(input())
 pontos = [tuple(map(int, input().split())) for _ in range(N)]
 pontos.insert(0,(0,0))
-distancias = [[0]*(N+1) for _ in range(N+1)]
-pares = [[] for _ in range(N+1)]
+distancias = []
 for i in range(N+1):
     for j in range(i+1,N+1):
-        distancia = (pontos[i][0]-pontos[j][0])**2 + (pontos[i][1]-pontos[j][1])**2
-        distancias[i][j] = distancias[j][i] = distancia
+        distancias.append(((pontos[i][0]-pontos[j][0])**2 + (pontos[i][1]-pontos[j][1])**2,i,j))
+distancias.sort()
+pontos = [[0,0,0] for _ in range(N+1)]
+for d,i,j in distancias:
+    if d != pontos[i][0]:
+        pontos[i][0] = d
+        pontos[i][1] = pontos[i][2]
+    if d != pontos[j][0]:
+        pontos[j][0] = d
+        pontos[j][1] = pontos[j][2]
+    if i == 0:
+        pontos[i][2] = max(pontos[i][2],pontos[j][1]+1)
+    else:
+        pontos[i][2] = max(pontos[i][2],pontos[j][1] +1)
+        pontos[j][2] = max(pontos[j][2],pontos[i][1] +1)
+        
+print(pontos[0][2])
+
+
 
