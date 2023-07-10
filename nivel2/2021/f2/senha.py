08bae8abb777d4a6b7555c787b9ed5bc0f5d8257
Zelnatal <elnatan.honorato@gmail.com>
2023-07-09 22:58:06 -0300

    Senha da Vó Zinha

--- nivel2/2021/f2/senha.py
+++ nivel2/2021/f2/senha.py
@@ -1,19 +1,20 @@
 # questão: https://olimpiada.ic.unicamp.br/pratique/p2/2021/f2/senha/
-# nota: 30/100
+# nota: 50/100
+
+from itertools import cycle
+
 n,m,k = map(int,input().split())
 n_array = list(input())
-m_array = [list(sorted(input())) for _ in range(m)]
-p = int(input()) 
-ma = 0
-while p > 0:
-    if k < p:
-        n_array[n_array.index('#')] = m_array[ma][-1]
-    else:
-        n_array[n_array.index('#')] = m_array[ma][p-1]
-    ma += 1
-    p -= k
-for i in range(ma,m):
-    n_array[n_array.index('#')] = m_array[i][0]
+m_array = [cycle(sorted(input()*(k**i))) for i in range(m-1,-1,-1)]
+p =  int(input()) 
+valores = [None]*m
 
+for _ in range(p):
+    for i,a in enumerate(m_array):
+        valores[i] = next(a)
+        
+for i in valores:
+    n_array[n_array.index('#')] = i
+    
 print(''.join(n_array))
         
